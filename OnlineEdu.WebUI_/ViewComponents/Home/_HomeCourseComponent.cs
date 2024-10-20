cd617d139688440b5b602c528c8a9cc7094773a6
SaglicakKadir <107749674+SaglicakKadir@users.noreply.github.com>
2024-10-20 18:40:23 +0300

    _homeCourseComponent ekledim ve paketleri güncelledim

---
+++ OnlineEdu.WebUI_/ViewComponents/Home/_HomeCourseComponent.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineEdu.WebUI_.DTOs.CourseDtos;
+using OnlineEdu.WebUI_.Helpers;
+
+namespace OnlineEdu.WebUI_.ViewComponents.Home
+{
+    public class _HomeCourseComponent:ViewComponent
+    {
+        private readonly HttpClient _client =HttpClientInstance.CreateClient();
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var values = await _client.GetFromJsonAsync<List<ResultCourseDto>>("courses/GetActiveCourses");
+            return View(values);
+        }
+    }
+}
