54e3813205e179665c83ce42431d8020b6c49e7e
SaglicakKadir <107749674+SaglicakKadir@users.noreply.github.com>
2024-12-14 18:10:12 +0300

    partialView alanları düzenlendi

---
+++ OnlineEdu.WebUI_/ViewComponents/UILayout/_UILayoutHeaderContactInfoComponent.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineEdu.WebUI_.DTOs.ContactDtos;
+using OnlineEdu.WebUI_.Helpers;
+
+namespace OnlineEdu.WebUI_.ViewComponents.UILayout
+{
+    public class _UILayoutHeaderContactInfoComponent:ViewComponent
+    {
+        private readonly HttpClient _client=HttpClientInstance.CreateClient();
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var values = await _client.GetFromJsonAsync<List<ResultContactDto>>("contacts");
+            return View(values);
+        }
+    }
+}
---
+++ OnlineEdu.WebUI_/ViewComponents/UILayout/_UILayoutHeaderSocialMediaComponent.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineEdu.WebUI_.DTOs.SocialMediaDtos;
+using OnlineEdu.WebUI_.Helpers;
+
+namespace OnlineEdu.WebUI_.ViewComponents.UILayout
+{
+    public class _UILayoutHeaderSocialMediaComponent:ViewComponent
+    {
+        private readonly HttpClient _client=HttpClientInstance.CreateClient();
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var values = await _client.GetFromJsonAsync<List<ResultSocialMediaDto>>("socialMedias");
+            return View(values);
+        }
+    }
+}
